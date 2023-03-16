15e932fa4fcd30822e1b9587c607e76a2435ba9b
that-ambuj <ambujs89@gmail.com>
2023-03-16 21:03:29 +0530

    remove exampleHtml

--- link/main/main.go
+++ link/main/main.go
@@ -7,28 +7,6 @@ import (
 	"os"
 )
 
-var exampleHtml = `
-<html>
-<head>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-  <h1>Social stuffs</h1>
-  <div>
-    <a href="https://www.twitter.com/joncalhoun">
-      Check me out on twitter
-	  <span>some span</span>
-      <i class="fa fa-twitter" aria-hidden="true"></i>
-    </a>
-    <a href="https://github.com/gophercises">
-      Gophercises is on <strong>Github</strong>!
-    </a>
-  </div>
-</body>
-</html>
-
-`
-
 func main() {
 	file, err := os.OpenFile("../ex3.html", os.O_RDWR, 0755)
 	if err != nil {
